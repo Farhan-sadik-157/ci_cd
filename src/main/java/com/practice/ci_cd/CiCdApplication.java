e25595e842a95ad0b5394e1e5b91ce411f434a87
Mohammad Farhan Sadik <farhan.sadik@bjitgroup.com>
2024-08-30 11:09:37 +0600

    Test run

--- src/main/java/com/practice/ci_cd/CiCdApplication.java
+++ src/main/java/com/practice/ci_cd/CiCdApplication.java
@@ -1,5 +1,6 @@
 package com.practice.ci_cd;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class CiCdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CiCdApplication.class, args);
-	}
+		System.out.println("CI_CD Application Started");
 
+	}
 }
