6cdaa0966163782c35d69031230c46a15540b0f7
Mohammad Farhan Sadik <farhan.sadik@bjitgroup.com>
2024-08-30 11:13:32 +0600

    added a test class

--- src/test/java/com/practice/ci_cd/CiCdApplicationTests.java
+++ src/test/java/com/practice/ci_cd/CiCdApplicationTests.java
@@ -3,11 +3,15 @@ package com.practice.ci_cd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.logging.Logger;
+
 @SpringBootTest
 class CiCdApplicationTests {
+	static Logger log = Logger.getLogger(CiCdApplicationTests.class.getName());
 
 	@Test
 	void contextLoads() {
+		log.info("Hello! THIS a demo project for CI_CD");
 	}
 
 }
